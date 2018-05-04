069c4151b922b7ce3623423d26aba03eb0d3a269
Dan Kiss <flowfast47@gmail.com>
2018-05-04 15:14:17 -0700

    add in a unique id to each product also allow product descriptions to be toggled

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,9 +1,9 @@
 $(function() {
 // used to display product detail
-  $('.single_product').click(function(){
-    alert('hey you clicked the product name')
-    $('.single_product_description').show()
-  });
+  // $('.single_product').click(function(){
+  //   alert('hey you clicked the product name')
+  //   $('.single_product_description').show()
+  // });
 
 // used to add item to cart
   $('#new_order_item input[type=submit]').click(function() {
