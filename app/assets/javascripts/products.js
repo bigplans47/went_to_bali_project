592c6c07ca612363c4f68e6e12b1e196ce032104
Dan Kiss <flowfast47@gmail.com>
2018-05-04 14:24:10 -0700

    add method to click on name or price of item and get alert

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,4 +1,10 @@
 $(function() {
+// used to display product detail
+  $('.single_product').click(function(){
+    alert('hey')
+  });
+
+// used to add item to cart
   $('#new_order_item input[type=submit]').click(function() {
     // alert('You clicked add to cart!')
     $(this).parent('form').submit()
