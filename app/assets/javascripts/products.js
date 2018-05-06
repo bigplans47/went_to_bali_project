6de923b263c0aa4625991ee0c65fdd35f241aaa4
Dan Kiss <flowfast47@gmail.com>
2018-05-06 11:24:49 -0700

    cleanup code

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,14 +1,7 @@
 $(function() {
-// used to display product detail
-  // $('.single_product').click(function(){
-  //   alert('hey you clicked the product name')
-  //   $('.single_product_description').show()
-  // });
-
 // used to add item to cart
   $('#new_order_item input[type=submit]').click(function() {
     // alert('You clicked add to cart!')
     $(this).parent('form').submit()
   });
-
 });
